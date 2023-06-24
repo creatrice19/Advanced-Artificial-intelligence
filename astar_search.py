2dc44880f6511049360bc2da2fe992e50c1618d9
creatrice19 <vengaladasu.ravi2@gmail.com>
2023-06-24 14:04:17 -0500

    first commit

---
+++ astar_search.py
@@ -0,0 +1,147 @@
+import sys
+import matplotlib.pyplot as plt
+
+from queue import PriorityQueue
+
+
+# Manhattan distance heuristic function
+# Defining a heuristic function that calculates the Manhattan distance between two points
+def manhattan_distance(pos1, pos2):
+    return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1])
+
+
+
+# Defining a helper function to print the maze
+def print_maze(maze):
+    for row in maze:
+        print("".join(row))
+
+# matplot function
+def plot_maze1(maze):
+    fig, ax = plt.subplots()
+    
+    for i in range(len(maze)):
+        for j in range(len(maze[i])):
+            if maze[i][j] == '%':
+                ax.add_patch(plt.Rectangle((j, i), 1, 1, facecolor='yellow'))
+            elif maze[i][j] == '.':
+                ax.add_patch(plt.Rectangle((j, i), 1, 1, facecolor='red',linestyle='dotted'))
+            elif maze[i][j] == 'P':
+                ax.add_patch(plt.Rectangle((j, i), 1, 1, facecolor='green'))
+
+    
+    ax.set_xlim(0, len(maze[0]))
+    ax.set_ylim(len(maze), 0)
+    ax.set_aspect('equal')
+    
+    ax.plot([0, 0, len(maze[0]), len(maze[0]), 0], 
+            [0, len(maze), len(maze), 0, 0], 
+            color='blue', linestyle='dotted')
+    plt.show()
+
+# A* search algorithm implementation
+def astar(maze, goal_states):
+    # Define the start and goal state
+    rows, cols = len(maze), len(maze[0])
+    start_state = None
+
+    # Find the start and goal state in the maze
+    for i in range(rows):
+        for j in range(cols):
+            if maze[i][j] == "P":
+                start_state = (i, j)
+                break
+            if start_state:
+                break
+    # Return None if either the starting or goal state were not found
+    if start_state is None:
+        return None, None, None, None, None
+
+    # Initializing the data structures for A* search
+    visited = set()
+    border = PriorityQueue()
+    g_scores = {start_state: 0}
+    f_scores = {start_state: manhattan_distance(start_state, goal_states[0])}
+    border.put((f_scores[start_state], start_state, []))
+    num_nodes_expanded = 0
+    max_tree_depth = 0
+    max_border_size = 1
+    remaining_goals = set(goal_states)
+
+    # Performing A* search
+    while not border.empty() and remaining_goals:
+        # Get the current node with the lowest f_score from the priority queue
+        _, curr_place, path = border.get()
+        num_nodes_expanded += 1
+        # Return the path if the current node is the goal position
+        if curr_place in goal_states:
+            return path, len(path), num_nodes_expanded, max_tree_depth, max_border_size
+
+
+        # Add the current node to the visited set if it has not been visited before
+        if curr_place not in visited:
+            visited.add(curr_place)
+            i, j = curr_place
+            # Generate the neighboring nodes of the current node
+            for x, y in [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]:
+                # Check if the neighboring node is valid and has not been visited before
+                if 0 <= x < rows and 0 <= y < cols and maze[x][y] != "%" and (x, y) not in visited:
+                    # Calculate the tentative g_score and update it if it is lower than the previous one
+                    tentative_g_score = g_scores[curr_place] + 1
+                    if (x, y) not in g_scores or tentative_g_score < g_scores[(x, y)]:
+                        g_scores[(x, y)] = tentative_g_score
+                        # Calculate the f_score and add the neighboring node to the priority queue
+                        f_scores[(x, y)] = tentative_g_score + manhattan_distance((x, y), goal_states[0])
+                        border.put((f_scores[(x, y)], (x, y), path + [(x, y)]))
+                        # Update the maximum fringe size and maximum tree depth
+                        max_border_size = max(max_border_size, border.qsize())
+                        max_tree_depth = max(max_tree_depth, len(path) + 1)
+
+    # If A* search fails to find a solution, it returns None
+    return None, None, num_nodes_expanded, max_tree_depth, max_border_size
+
+
+# A* search algorithm implementation
+def run_astar(file_list):
+    for file_name in file_list:
+        print('\n')
+        with open(file_name, "r") as file:
+            maze = [list(line.strip()) for line in file]
+        # Find the dimensions of the maze
+        rows, cols = len(maze), len(maze[0])
+
+        goal_states = []
+        for i in range(rows):
+            for j in range(cols):
+                if maze[i][j] == ".":
+                    goal_states.append((i, j))
+        path, path_len, num_nodes_expanded, max_tree_depth, max_border_size = astar(maze, goal_states)
+
+        path, path_len, num_nodes_expanded, max_tree_depth, max_border_size = astar(maze, goal_states)
+
+        if path is None:
+            print(f"No path found for {file_name}.")
+        else:
+            print(f"Path found for {file_name}:")
+            print("Path length:", path_len)
+            print("Number of nodes expanded:", num_nodes_expanded)
+            
+            # Mark the visited squares on the path in the maze
+            for step in path:
+                i, j = step
+                maze[i][j] = "."
+
+            # Print the updated maze
+            print_maze(maze)
+            plot_maze1(maze)
+            
+
+
+def main():
+    file_list = ['bigMaze.lay', 'openMaze.lay', 'mediumMaze.lay', 'tinySearch.lay', 'trickySearch.lay',
+                 'smallSearch.lay']
+    run_astar(file_list)
+
+
+if __name__ == "__main__":
+    main()
---
+++ main.py
@@ -0,0 +1,130 @@
+import sys
+import matplotlib.pyplot as plt
+from collections import deque
+
+from astar_search import *
+
+
+# Function to read a maze from the given file
+def read_maze(filename):
+    with open(filename) as file:
+        maze = [[char for char in line.strip()] for line in file]
+    return maze
+
+class State:
+    def __init__(self, coordinates, path):
+        self.coordinates = coordinates
+        self.path = path
+
+
+# Function to visualize the solution path in the maze
+def visualize_solution(maze, path):
+    for i, j in path:
+        maze[i][j] = '.'
+    for row in maze:
+        print(' '.join(row))
+    print()
+
+
+# Breadth-first search algorithm implementation
+def bfs(maze):
+    # Defining the start state
+    rows, cols = len(maze), len(maze[0])
+    start_state = None
+    goal_states = []
+
+    for i, row in enumerate(maze):
+        for j, char in enumerate(row):
+            if char == "P":
+                start_state = State((i, j),[])
+            elif char == ".":
+                goal_state = State((i, j),[])
+                goal_states.append(goal_state)
+
+    if start_state is None or not goal_states:
+        return None, None, None, None, None
+
+    # Initializing the data structures for Breadth-first search
+    visited = set()
+    border = deque()
+    border.append(start_state)
+    num_nodes_expanded = 0
+    max_tree_depth = 0
+    max_border_size = 1
+
+    # Performing Breadth-first search
+    while border:
+        curr_state = border.popleft()
+        num_nodes_expanded += 1
+
+        for goal_state in goal_states:
+            if curr_state.coordinates == goal_state.coordinates:
+                goal_states.remove(goal_state)  # Remove the goal state from the list once reached
+                if not goal_states:  # Check if all goal states have been reached
+                    return curr_state.path, len(curr_state.path), num_nodes_expanded, max_tree_depth, max_border_size
+                
+        if curr_state.coordinates not in visited:
+            visited.add(curr_state.coordinates)
+            i, j = curr_state.coordinates
+            for x, y in [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]:
+                if 0 <= x < rows and 0 <= y < cols and maze[x][y] != "%" and (x, y) not in visited:
+                    new_state = State((x, y), curr_state.path + [(x, y)])
+                    border.append(new_state)
+                    max_border_size = max(max_border_size, len(border))
+            max_tree_depth = max(max_tree_depth, len(curr_state.path))
+
+    # If BFS fails to find a solution, it returns None
+    return None, None, num_nodes_expanded, max_tree_depth, max_border_size
+
+# matplot function
+def plot_maze1(maze):
+    fig, ax = plt.subplots()
+    
+    for i in range(len(maze)):
+        for j in range(len(maze[i])):
+            if maze[i][j] == '%':
+                ax.add_patch(plt.Rectangle((j, i), 1, 1, facecolor='yellow'))
+            elif maze[i][j] == 'P':
+                ax.add_patch(plt.Rectangle((j, i), 1, 1, facecolor='green'))
+            elif maze[i][j] == '.':
+                ax.add_patch(plt.Rectangle((j, i), 1, 1, facecolor='red',linestyle='dotted'))
+            
+     
+    
+    ax.set_xlim(0, len(maze[0]))
+    ax.set_ylim(len(maze), 0)
+    ax.set_aspect('equal')
+    
+    ax.plot([0, 0, len(maze[0]), len(maze[0]), 0], 
+            [0, len(maze), len(maze), 0, 0], 
+            color='blue', linestyle='dotted')
+    plt.show()
+
+# Main function to run BFS on a given maze and writing output to a file
+def run_bfs():
+    file_names = ['smallMaze.lay', 'mediumMaze.lay', 'bigMaze.lay', 'openMaze.lay','tinySearch.lay', 'trickySearch.lay','smallSearch.lay']
+    for file in file_names:
+        print('\n')
+        maze = read_maze(file)
+        sol, path_cost, num_nodes_expanded, max_tree_depth, max_border_size = bfs(maze)
+        if sol is not None:
+            print("BFS solution:", file, "with path cost of", path_cost)
+            print("Number of nodes expanded:", num_nodes_expanded)
+            print("Maximum tree depth searched:", max_tree_depth)
+            print("Maximum size of the fringe:", max_border_size)
+            print("Visualizing solution:")
+            visualize_solution(maze, sol)
+            plot_maze1(maze)
+        else:
+            print("BFS failed to find a solution", file=sys.stderr)
+
+
+if __name__ == "__main__":
+    print('=' * 130)
+    print("starting Breadth-first search (BFS)...")
+    print('=' * 130)
+    run_bfs()
+    print('=' * 130)
+    print("starting A* search...")
+    print('=' * 130)
+    main()
